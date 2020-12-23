d101a025f8faf9733afadfc9882ea4a7cc51bc5d
ekuecks <kueckse@gmail.com>
2020-12-23 10:50:57 -0800

    Remove unsafe from 23

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -1,46 +1,48 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 struct Node {
     val: u64,
-    next: Option<*mut Node>,
+    next: Option<Rc<RefCell<Node>>>,
 }
 
 fn main() {
-    let mut nodes: HashMap<u64, *mut Node> = HashMap::new();
-    let cur = Box::new(Node { val: 1, next: None });
-    let mut ptr: *mut Node = Box::into_raw(cur);
-    nodes.insert(1, ptr);
+    let mut nodes: HashMap<u64, Rc<RefCell<Node>>> = HashMap::new();
+    let mut cur = Rc::new(RefCell::new(Node { val: 1, next: None }));
+    nodes.insert(1, Rc::clone(&cur));
     for val in &[6, 7, 2, 4, 8, 3, 5, 9] {
-        let next = Box::new(Node {
+        let next = Rc::new(RefCell::new(Node {
             val: *val,
             next: None,
-        });
-        let next_ptr: *mut Node = Box::into_raw(next);
-        unsafe { (*ptr).next = Some(next_ptr) };
-        ptr = next_ptr;
-        nodes.insert(*val, ptr);
+        }));
+        cur.borrow_mut().next = Some(Rc::clone(&next));
+        cur = next;
+        nodes.insert(*val, Rc::clone(&cur));
     }
     for val in 10..=1000000 {
-        let next = Box::new(Node { val, next: None });
-        let next_ptr: *mut Node = Box::into_raw(next);
-        unsafe { (*ptr).next = Some(next_ptr) };
-        ptr = next_ptr;
-        nodes.insert(val, ptr);
+        let next = Rc::new(RefCell::new(Node {
+            val: val,
+            next: None,
+        }));
+        cur.borrow_mut().next = Some(Rc::clone(&next));
+        cur = next;
+        nodes.insert(val, Rc::clone(&cur));
     }
-    let head_ptr: *mut Node = *nodes.get(&1).unwrap();
-    unsafe { (*ptr).next = Some(head_ptr) };
+    let head_ptr = nodes.get(&1).unwrap();
+    cur.borrow_mut().next = Some(Rc::clone(head_ptr));
 
-    let mut cur_node = *nodes.get(&1).unwrap();
+    let mut cur_node = Rc::clone(nodes.get(&1).unwrap());
     for j in 0..10_000_000 {
         if j % 100_000 == 0 {
             println!("{}", j);
         }
-        let value = unsafe { (*cur_node).val };
+        let value = cur_node.borrow().val.clone();
         let mut to_remove = Vec::new();
         for _ in 0..3 {
-            let to_insert = unsafe { (*cur_node).next.unwrap() };
-            unsafe { (*cur_node).next = (*to_insert).next };
-            to_remove.push(to_insert);
+            let to_insert = Rc::clone(cur_node.borrow().next.as_ref().unwrap());
+            cur_node.borrow_mut().next = Some(Rc::clone(to_insert.borrow().next.as_ref().unwrap()));
+            to_remove.push(Rc::clone(&to_insert));
         }
         let mut target_value = value - 1;
         loop {
@@ -49,7 +51,7 @@ fn main() {
             }
             let mut found = false;
             for removed in to_remove.iter() {
-                if unsafe { (**removed).val == target_value } {
+                if removed.borrow().val == target_value {
                     target_value -= 1;
                     found = true;
                     break;
@@ -59,18 +61,29 @@ fn main() {
                 break;
             }
         }
-        let mut insert_node = *nodes.get(&target_value).unwrap();
-        let stored = unsafe { (*insert_node).next.unwrap() };
-        for removed in to_remove {
-            unsafe { (*insert_node).next = Some(removed) };
+        let mut insert_node = nodes.get(&target_value).unwrap();
+        let stored = Rc::clone(insert_node.borrow().next.as_ref().unwrap());
+        for removed in to_remove.iter() {
+            insert_node.borrow_mut().next = Some(Rc::clone(removed));
             insert_node = removed;
         }
-        unsafe { (*insert_node).next = Some(stored) };
-        cur_node = unsafe { (*cur_node).next.unwrap() };
+        insert_node.borrow_mut().next = Some(stored);
+        let next_node = Rc::clone(cur_node.borrow_mut().next.as_ref().unwrap());
+        cur_node = next_node;
     }
-    let cur_node = *nodes.get(&1).unwrap();
-    let n_v = unsafe { (*(*cur_node).next.unwrap()).val };
-    let nn_v = unsafe { (*(*(*cur_node).next.unwrap()).next.unwrap()).val };
+    let cur_node = nodes.get(&1).unwrap();
+    let n_v = cur_node.borrow().next.as_ref().unwrap().borrow().val;
+    let nn_v = cur_node
+        .borrow()
+        .next
+        .as_ref()
+        .unwrap()
+        .borrow()
+        .next
+        .as_ref()
+        .unwrap()
+        .borrow()
+        .val;
     println!("{}, {}", n_v, nn_v);
-    println!("{}", n_v * nn_v);
+    println!("Answer: {}", n_v * nn_v);
 }
